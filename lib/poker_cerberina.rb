4c3346e522c84c5a882f7d0e72257a0638bd443b
cerberina <cerberina25@gmail.com>
2023-03-15 19:15:14 +0100

    First draft of searching pairs, sets and quads

--- lib/poker_cerberina.rb
+++ lib/poker_cerberina.rb
@@ -1,23 +1,93 @@
 module PokerCerberina
+  SUIT = ["♠︎", "♣︎", "♥︎", "♦︎"]
+  RANK = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 end
 
 class PokerCerberina::Deck
+  def initialize
+    @deck = []
+    PokerCerberina::SUIT.each do |s|
+      PokerCerberina::RANK.each do |r|
+        @deck << PokerCerberina::Card.new(r, s)
+      end
+    end
+  end
+
+  def size
+    @deck.size
+  end
+
+  def print_deck
+    @deck.each { |d| puts d.print_card }
+  end
+
+  def shuffle
+    @deck.shuffle!
+  end
 end
 
 class PokerCerberina::Card
   include Comparable
   attr_reader :rank, :suit
-  SUIT = ["D", "H", "S", "C"]
-  RANK = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
   def initialize(rank, suit)
-    raise "Invalid value for rank" unless RANK.include? rank
+    raise "Invalid value for rank" unless PokerCerberina::RANK.include? rank
     @rank = rank
-    raise "Invalid value for suit" unless SUIT.include? suit
+    raise "Invalid value for suit" unless PokerCerberina::SUIT.include? suit
     @suit = suit
   end
 
   def <=>(other)
-    RANK.index(rank) <=> RANK.index(other.rank)
+    PokerCerberina::RANK.index(rank) <=> PokerCerberina::RANK.index(other.rank)
+  end
+
+  def print_card
+    puts "#{@rank}#{@suit}"
+  end
+end
+
+def find_pairs(cards)
+  groups = {}
+  hand.each do |c|
+    if groups[c.rank]
+      groups[c.rank] << c
+    else
+      groups[c.rank] = [c]
+    end
+  end
+  result = false
+  groups.each_value do |value|
+    if value.size == 2
+      value.each { |v| v.print_card }
+    end
+  end
+  result
+end
+
+def search_combinations
+  hand = PokerCerberina::Deck.new.shuffle[0..4]
+  #hand = [PokerCerberina::Card.new("K", "♠︎"), PokerCerberina::Card.new("K", "♣︎"), PokerCerberina::Card.new("K", "♥︎"), PokerCerberina::Card.new("2", "♠︎"), PokerCerberina::Card.new("2", "♣︎")]
+  groups = {}
+  hand.each do |c|
+    if groups[c.rank]
+      groups[c.rank] << c
+    else
+      groups[c.rank] = [c]
+    end
+  end
+  groups.each_value do |value|
+    if value.size == 2
+      puts "__________________________________"
+      puts "It's a pair!"
+      value.each { |v| v.print_card }
+    elsif value.size == 3
+      puts "__________________________________"
+      puts "It's a set!"
+      value.each { |v| v.print_card }
+    elsif value.size == 4
+      puts "__________________________________"
+      puts "It's a quads!"
+      value.each { |v| v.print_card }
+    end
   end
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -4,13 +4,13 @@ require "poker_cerberina"
 describe PokerCerberina::Card do
   describe "#initialize" do
     it "should raise an exception for empty rank" do
-      expect { PokerCerberina::Card.new("", "S") }.to raise_error("Invalid value for rank")
+      expect { PokerCerberina::Card.new("", "♦︎") }.to raise_error("Invalid value for rank")
     end
     it "should raise an exception for empty suit" do
       expect { PokerCerberina::Card.new("K", "") }.to raise_error("Invalid value for suit")
     end
     it "should raise an exception for invalid rank" do
-      expect { PokerCerberina::Card.new("1", "D") }.to raise_error("Invalid value for rank")
+      expect { PokerCerberina::Card.new("1", "♥︎") }.to raise_error("Invalid value for rank")
     end
     it "should raise exception for invalid suit" do
       expect { PokerCerberina::Card.new("9", "X") }.to raise_error("Invalid value for suit")
@@ -19,26 +19,26 @@ describe PokerCerberina::Card do
 
   describe "#<=>" do
     it "is equal for cards with same rank" do
-      dama_s = PokerCerberina::Card.new("Q", "S")
-      dama_d = PokerCerberina::Card.new("Q", "D")
-      expect(dama_s == dama_d).to eq true
+      card_a = PokerCerberina::Card.new("Q", "♦︎")
+      card_b = PokerCerberina::Card.new("Q", "♥︎")
+      expect(card_a == card_b).to eq true
     end
     it "shouldn't be equal for cards with different rank but same suit" do
-      dama_s = PokerCerberina::Card.new("Q", "S")
-      valet_s = PokerCerberina::Card.new("J", "S")
-      expect(dama_s == valet_s).to eq false
+      card_a = PokerCerberina::Card.new("Q", "♦︎")
+      card_b = PokerCerberina::Card.new("J", "♦︎")
+      expect(card_a == card_b).to eq false
     end
 
     context "compare cards with different ranks" do
       it "should returns false" do
-        ace_h = PokerCerberina::Card.new("A", "H")
-        two_c = PokerCerberina::Card.new("2", "C")
-        expect(two_c > ace_h).to eq false
+        card_a = PokerCerberina::Card.new("A", "♣︎")
+        card_b = PokerCerberina::Card.new("2", "♠︎")
+        expect(card_b > card_a).to eq false
       end
       it "should return true" do
-        ace_h = PokerCerberina::Card.new("A", "H")
-        two_c = PokerCerberina::Card.new("2", "C")
-        expect(two_c < ace_h).to eq true
+        card_a = PokerCerberina::Card.new("A", "♣︎")
+        card_b = PokerCerberina::Card.new("2", "♠︎")
+        expect(card_b < card_a).to eq true
       end
     end
   end
